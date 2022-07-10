d3ab4c427e7e3a4c693f989f7d353822a6cc51e7
Tainá Santana <tainnaps@gmail.com>
2022-07-10 17:24:14 -0300

    feat: create generic MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,36 @@
+import { Model as MongooseModel } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+  protected _mongooseModel: MongooseModel<T>;
+
+  constructor(mongooseModel: MongooseModel<T>) {
+    this._mongooseModel = mongooseModel;
+  }
+
+  async create(data: T): Promise<T> {
+    return this._mongooseModel.create({ ...data });
+  }
+
+  async read(): Promise<T[]> {
+    return this._mongooseModel.find();
+  }
+
+  async readOne(id: string): Promise<T | null> {
+    return this._mongooseModel.findOne({ _id: id });
+  }
+
+  async update(id: string, data: T): Promise<T | null> {
+    return this._mongooseModel.findOneAndUpdate(
+      { _id: id },
+      { ...data },
+      { new: true },
+    );
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this._mongooseModel.findOneAndDelete({ _id: id });
+  }
+}
+
+export default MongoModel;
