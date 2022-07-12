8a4639143679549d52808fc4f8e6d394f6a13d37
Tainá Santana <tainnaps@gmail.com>
2022-07-12 02:16:38 -0300

    feat: create not found error

---
+++ src/errors/NotFoundError.ts
@@ -0,0 +1,9 @@
+import CustomError from './CustomError';
+
+class NotFoundError extends CustomError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export default NotFoundError;
