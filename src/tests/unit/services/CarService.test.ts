7f856cc01a9e279b4aa407f2be25f4721953011a
Tainá Santana <tainnaps@gmail.com>
2022-07-13 01:43:56 -0300

    test: add car service read test case

--- src/tests/unit/services/CarService.test.ts
+++ src/tests/unit/services/CarService.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import CarModelMock from '../../mocks/CarModelMock';
 import CarService from '../../../services/CarService';
-import { NEW_CAR_MOCK } from '../../mocks/CarMock';
+import { CAR_MOCK, CARS_MOCK } from '../../mocks/CarMock';
 
 describe('CarService', () => {
   const carModelMock = new CarModelMock();
@@ -10,10 +10,18 @@ describe('CarService', () => {
   describe('when creating a new car', () => {
     describe('with valid data', () => {
       it('should resolve the promise with the new car data', async () => {
-        const createdCar = await carService.create(NEW_CAR_MOCK);
+        const createdCar = await carService.create(CAR_MOCK);
 
-        expect(createdCar).to.be.deep.equal(NEW_CAR_MOCK);
+        expect(createdCar).to.be.deep.equal(CAR_MOCK);
       });
+    });
+  });
+
+  describe('when reading all cars', () => {
+    it('should resolve the promise with all the cars', async () => {
+      const cars = await carService.read();
+
+      expect(cars).to.be.deep.equal(CARS_MOCK);
     });
   });
 });
