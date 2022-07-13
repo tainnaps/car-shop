895bdd991bf50d252833807dcd48a621eb22d2b5
Tainá Santana <tainnaps@gmail.com>
2022-07-13 02:27:05 -0300

    fix: remove id from car mocks

--- src/tests/mocks/CarMock.ts
+++ src/tests/mocks/CarMock.ts
@@ -1,5 +1,4 @@
 export const CAR_MOCK = {
-  _id: '62cb27d1b8e70fe59b08186c',
   model: 'Mocked model',
   year: 2005,
   color: 'blue',
@@ -11,7 +10,6 @@ export const CAR_MOCK = {
 
 export const CARS_MOCK = [
   {
-    _id: '62cb27d1b8e70fe59b08186c',
     model: 'Mocked model',
     year: 2005,
     color: 'blue',
@@ -21,7 +19,6 @@ export const CARS_MOCK = [
     seatsQty: 5,
   },
   {
-    _id: '62cb27d1bf478f7ffgy8g86c',
     model: 'Another mocked model',
     year: 2004,
     color: 'green',
